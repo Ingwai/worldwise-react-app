d26d370133217594024cce08113f4539d3c3b948
Ingwai <ingwai@wp.pl>
2023-08-05 19:47:31 +0200

    add authenticated and protected routes

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,8 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import styles from './PageNav.module.css';
 import Logo from './Logo';
+import { useAuth } from '../contexts/FakeAuthContext';
 
 const PageNav = () => {
+	const { isAuthenticated } = useAuth();
 	return (
 		<nav className={styles.nav}>
 			<Logo />
@@ -15,7 +17,7 @@ const PageNav = () => {
 				</li>
 				<li>
 					<NavLink to='/login' className={styles.ctaLink}>
-						Login
+						{`${!isAuthenticated ? 'Login' : 'Logout'}`}
 					</NavLink>
 				</li>
 			</ul>
